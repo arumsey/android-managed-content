efdf531996b3c956f89d4be3b30ab98f42bb1f86
Anthony Rumsey <arumsey@adobe.com>
2014-05-27 10:12:06 -0400

    added location component

--- content/src/main/content/jcr_root/apps/homepage-webview-app/components/phonegap-location/clientlibs/phonegap-location.js
+++ content/src/main/content/jcr_root/apps/homepage-webview-app/components/phonegap-location/clientlibs/phonegap-location.js
@@ -2,11 +2,54 @@
 
     angular.module('phonegapLocation', ["cqServices", "cqMaps"])
 
-        .controller('LocationCtrl', ['$scope', '$timeout', 'cqDeviceUtils', function($scope, $timeout, cqDeviceUtils) {
+        .factory('locationService', function () {
+
+            /** Converts numeric degrees to radians */
+            if (typeof Number.prototype.toRad == 'undefined') {
+                Number.prototype.toRad = function() {
+                    return this * Math.PI / 180;
+                }
+            }
+
+            function calculateDistance(start, end) {
+                var d = 0;
+                if (start && end) {
+                    var R = 6371;
+                    var lat1 = start.lat.toRad(), lon1 = start.lng.toRad();
+                    var lat2 = end.lat.toRad(), lon2 = end.lng.toRad();
+                    var dLat = lat2 - lat1;
+                    var dLon = lon2 - lon1;
+
+                    var a = Math.sin(dLat/2) * Math.sin(dLat/2) +
+                        Math.cos(lat1) * Math.cos(lat2) *
+                            Math.sin(dLon/2) * Math.sin(dLon/2);
+                    var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+                    d = R * c;
+                }
+                return d;
+            }
+
+            return {
+
+                getDistance: function(start, end) {
+                    return calculateDistance(start, end);
+                }
+            };
+        })
+
+        .controller('LocationCtrl', ['$scope', '$timeout', 'cqDeviceUtils', 'locationService', function($scope, $timeout, cqDeviceUtils, locationService) {
 
             $scope.showMap = false;
             $scope.origin = null;
             $scope.locations = [];
+
+            //conference location
+            $scope.locations.push({
+                "coordinates": {
+                    "lat": 47.5542490,
+                    "lng": 7.5893580
+                }
+            });
 
             //get current position
             cqDeviceUtils.getPosition(function(position) {
@@ -14,21 +57,17 @@
                     lat: position.coords.latitude,
                     lng: position.coords.longitude
                 };
-                $scope.locations.push({
-                    "coordinates": {
-                        "lat": position.coords.latitude,
-                        "lng": position.coords.longitude
-                    }
-                });
-                //reverse geocode
+                $scope.distance = locationService.getDistance($scope.origin, $scope.locations[0].coordinates);
+
+                //reverse geocode current location
                 var latlng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
                 var geocoder = new google.maps.Geocoder();
                 geocoder.geocode({'latLng': latlng}, function(results, status) {
                     if (status == google.maps.GeocoderStatus.OK) {
                         if (results[1]) {
-                            $scope.locations[0].label = results[1].formatted_address;
+                            $scope.origin.label = results[1].formatted_address;
                         } else {
-                            $scope.locations[0].label = "No location found";
+                            $scope.origin.label = "No location found";
                         }
                         $scope.$apply();
                     } else {
--- content/src/main/content/jcr_root/apps/homepage-webview-app/components/phonegap-location/phonegap-location.jsp
+++ content/src/main/content/jcr_root/apps/homepage-webview-app/components/phonegap-location/phonegap-location.jsp
@@ -3,9 +3,10 @@
 %>
 <div class="location-details" ng-controller="LocationCtrl">
     <div class="location-container">
-        <h3>{{locations[0].label}}</h3>
+        <h3 ng-hide>Your current location is: {{origin.label}}</h3>
+        <p ng-hide>You are <b>{{distance|number:2}} km</b> away from the venue.</p>
         <div class="location-map">
-            <cq-map zoom="16" maptype="roadmap" center="origin" markers="locations" refresh="showMap">
+            <cq-map zoom="12" maptype="roadmap" center="origin" markers="locations" refresh="showMap">
                 Loading map...
             </cq-map>
         </div>
