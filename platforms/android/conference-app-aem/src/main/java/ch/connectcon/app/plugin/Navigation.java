3d9489161b34052e550dcaa768a54383aeca2f1a
Anthony Rumsey <arumsey@adobe.com>
2014-06-24 23:24:18 +0200

    demo: step 5: Navigation - implement CordovaPlugin

--- platforms/android/conference-app-aem/src/main/java/ch/connectcon/app/plugin/Navigation.java
+++ platforms/android/conference-app-aem/src/main/java/ch/connectcon/app/plugin/Navigation.java
@@ -17,9 +17,44 @@ public class Navigation extends CordovaPlugin {
 
     @Override
     public boolean execute(String action, CordovaArgs args, final CallbackContext callbackContext) throws JSONException {
+        if ("navigate".equals(action)) {
+            navigate(args, callbackContext);
+            return true;
+        }
         return false;
     }
 
-    //TODO: add cordova plugin
+    private void navigate(final CordovaArgs args, final CallbackContext callbackContext) {
+        this.cordova.getActivity().runOnUiThread(new Runnable() {
+            public void run() {
+                navigateSync(args, callbackContext);
+            }
+        });
+    }
 
+    private void navigateSync(CordovaArgs args, final CallbackContext ctx) {
+        try {
+            MainActivity homeActivity = (MainActivity) this.cordova.getActivity();
+            Context context = homeActivity.getApplicationContext();
+
+            String bannerType = args.getString(0);
+            String bannerValue = args.getString(1);
+
+            if (bannerType.equals("INTERNAL_NAV")) {
+                homeActivity.goToPage(Integer.valueOf(bannerValue).intValue());
+            } else if (bannerType.equals("EXTERNAL_LINK")) {
+                Intent browserIntent = new Intent(Intent.ACTION_VIEW);
+                browserIntent.setData(Uri.parse(bannerValue));
+                browserIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                context.startActivity(browserIntent);
+            }
+
+            ctx.success();
+
+        } catch (Exception e) {
+            String errorMessage = "An error occurred during navigation.";
+            ctx.error(errorMessage);
+            Log.e(LOG_TAG, errorMessage, e);
+        }
+    }
 }
